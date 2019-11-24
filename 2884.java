31b60dc18604698f680b1c9cb513d6d76b028c6c
jeongauuu <57554778+jeongauuu@users.noreply.github.com>
2019-11-24 16:35:45 +0900

    Add files via upload

---
+++ 2884.java
@@ -0,0 +1,17 @@
+﻿import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        int H = scanner.nextInt();
+		int M = scanner.nextInt();
+        
+        int n = H*60+M-45;
+        
+        if(n<0){
+        	n += 1440;
+        }
+        System.out.printf("%d %d", n/60, n%60 );
+    }    
+}
